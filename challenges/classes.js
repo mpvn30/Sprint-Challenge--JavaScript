4e7bd24b102cd94f2f8833a4fdec5477d3115994
Mike Nguyen <mpvnguyen1996@gmail.com>
2019-08-23 12:33:14 -0400

    finished stretch goals

--- challenges/classes.js
+++ challenges/classes.js
@@ -24,3 +24,26 @@ console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+
+class CubeMaker extends CuboidMaker{
+    constructor(cubeAttribute){
+        super(cubeAttribute)
+    }
+    volume(){
+    //V=a3
+        return Math.pow(this.length, 3);
+    }
+    //A=6a2
+    surfaceArea(){
+        return (Math.pow(this.length, 2)) * 6;    
+    }
+}
+
+const cube = new CubeMaker({
+    length: 4,
+    width: 5,
+    height: 5
+  });
+
+console.log(cube.volume());
+console.log(cube.surfaceArea());
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -96,7 +96,7 @@ console.log(contactInfo);
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
   for (i = 0; i < graduates.length; i++){
-    if (graduates[i].university === "Uni"){
+    if (graduates[i].university.includes("Uni")){
       unisWithUni.push(graduates[i].university);
     }
   }
